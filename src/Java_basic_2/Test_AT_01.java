4739462e5a44895424297f695e0a9ab4321839d1
pc <gardiakshay@gmail.com>
2025-05-14 17:24:17 +0530

    Addition

---
+++ src/Java_basic_2/Test_AT_01.java
@@ -0,0 +1,12 @@
+package Java_basic_2;
+
+public class Test_AT_01 {
+    public static void main(String[] args) {
+
+        byte a= 12;
+        byte b= 16;
+        int c= a+b;
+
+        System.out.println("Addition of two no is :"+c);
+    }
+}

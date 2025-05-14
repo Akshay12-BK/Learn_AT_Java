011fe99fdc6caf1810fd3fb77683964a8b159546
pc <gardiakshay@gmail.com>
2025-05-14 17:32:06 +0530

    Multiplication

---
+++ src/Java_basic_2/Test_Multiplication.java
@@ -0,0 +1,16 @@
+package Java_basic_2;
+
+public class Test_Multiplication {
+    public static void main(String[] args) {
+
+        int a= 10;
+        int b= 15;
+        int c= a*b;
+        int d= b-a;
+        int k= a+b;
+        System.out.println("Multiplication of two value is: "+c);
+        System.out.println("Substraction of two value is: "+d);
+        System.out.println("Addition of two value is: "+k);
+
+    }
+}

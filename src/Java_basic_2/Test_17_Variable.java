4b61e1d09887ea9b2ef66cf21ee4c141713d5791
pc <gardiakshay@gmail.com>
2025-05-20 16:53:01 +0530

    Variable

---
+++ src/Java_basic_2/Test_17_Variable.java
@@ -0,0 +1,10 @@
+package Java_basic_2;
+
+public class Test_17_Variable {
+    public static void main(String[] args) {
+        System.out.println("Age 65");
+
+        int age= 25;
+        System.out.println("My age is :"+age);
+    }
+}

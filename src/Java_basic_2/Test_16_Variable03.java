5270415b49f77fd1eaa0c000afa306f4392ac026
pc <gardiakshay@gmail.com>
2025-05-20 16:58:26 +0530

    Variable

---
+++ src/Java_basic_2/Test_16_Variable03.java
@@ -0,0 +1,10 @@
+package Java_basic_2;
+
+public class Test_16_Variable03 {
+    public static void main(String[] args) {
+        int a= 15;
+        a= a+20;
+        a= 80;
+        System.out.println(a);
+    }
+}

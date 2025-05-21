6039381e912bb784b5e39cf2790c5e7e87f46784
pc <gardiakshay@gmail.com>
2025-05-21 15:46:03 +0530

    Variable

---
+++ src/Java_basic_2/Test_22_Variable_boolean.java
@@ -0,0 +1,12 @@
+package Java_basic_2;
+
+public class Test_22_Variable_boolean {
+    public static void main(String[] args) {
+
+        boolean is_Akshay_married = true;
+        boolean Akshay_Age_IS_Greater_Than_18 = true;
+        boolean Long_Name_Is_Allowed = true;
+
+
+    }
+}

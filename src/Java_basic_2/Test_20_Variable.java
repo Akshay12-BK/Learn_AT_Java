a93db70b72f81f7f25c288912512bcd4e5d7bd2f
pc <gardiakshay@gmail.com>
2025-05-21 14:58:14 +0530

    Variable

---
+++ src/Java_basic_2/Test_20_Variable.java
@@ -0,0 +1,19 @@
+package Java_basic_2;
+
+public class Test_20_Variable {
+
+    public static void main(String[] args) {
+        int _2 = 110;
+        System.out.println(_2);
+
+        int _1= 23;
+        int _0= 50;
+
+        System.out.println(_1 + _0 );
+
+    }
+
+
+
+
+}

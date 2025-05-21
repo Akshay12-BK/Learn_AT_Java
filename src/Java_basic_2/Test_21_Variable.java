cff9d85e70b0eb2a6b464cfb45bc7be724026073
pc <gardiakshay@gmail.com>
2025-05-21 15:16:31 +0530

    Variable

---
+++ src/Java_basic_2/Test_21_Variable.java
@@ -0,0 +1,23 @@
+package Java_basic_2;
+
+public class Test_21_Variable {
+
+    public static void main(String[] args) {
+    //    int enum =20;   It is a invalid, enum is a keyword
+        int Enum = 40;  // valid E is written in capital letter so it's not keyword
+
+        int a_b = 34;
+
+        System.out.println(Enum);
+        System.out.println(a_b);
+
+
+    }
+
+
+
+
+
+
+
+}

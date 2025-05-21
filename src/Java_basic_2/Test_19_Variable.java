13cb2d3e2d6fbb3ce2fdb37cd33aa6145b33e6a9
pc <gardiakshay@gmail.com>
2025-05-21 14:55:26 +0530

    Variable

---
+++ src/Java_basic_2/Test_19_Variable.java
@@ -0,0 +1,21 @@
+package Java_basic_2;
+
+public class Test_19_Variable {
+
+    public static void main(String[] args) {
+
+//        int _ = 65; valid sentence but we can't run
+        int $ = 65;  // valid
+        int year = 35;  // valid
+        int this_is_a_valid_Name = 45;  // valid
+
+
+        System.out.println($);
+        System.out.println(year);
+        System.out.println(this_is_a_valid_Name);
+
+
+
+
+    }
+}

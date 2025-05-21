baf138e144ff77c6daedc1b5cf3031c376c46442
pc <gardiakshay@gmail.com>
2025-05-21 15:28:22 +0530

    Variable

--- src/Java_basic_2/Test_21_Variable.java
+++ src/Java_basic_2/Test_21_Variable.java
@@ -7,17 +7,15 @@ public class Test_21_Variable {
         int Enum = 40;  // valid E is written in capital letter so it's not keyword
 
         int a_b = 34;
+     //   int Akshay gardi = 55;  Not allowed space
+        int Akshay_gardi = 25;
+        int Test_21_Variable = 45;
 
         System.out.println(Enum);
         System.out.println(a_b);
-
+        System.out.println(Akshay_gardi);
+        System.out.println(Test_21_Variable);
 
     }
-
-
-
-
-
-
 
 }

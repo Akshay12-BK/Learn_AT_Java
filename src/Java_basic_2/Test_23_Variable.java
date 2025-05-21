b2c728bb4e2b370efdc9a051328b93a5491649e6
pc <gardiakshay@gmail.com>
2025-05-21 15:46:35 +0530

    Variable

---
+++ src/Java_basic_2/Test_23_Variable.java
@@ -0,0 +1,31 @@
+package Java_basic_2;
+
+public class Test_23_Variable {
+    public static void main(String[] args) {
+
+     //   byte b= 128;  //  -128 to 127
+        byte b= 10;
+        int i = 10;
+        short s= 10;
+        char c= 'a';
+        long l = 8527419655l;
+        long l1= 8564475245l;
+
+        int salary = 15555;
+        long sal = 41555;
+
+        float f = 3.14f;
+        double d = 91.30;
+
+        System.out.println(b);
+        System.out.println(sal);
+        System.out.println(salary);
+        System.out.println(f);
+        System.out.println(d);
+
+
+
+
+
+    }
+}

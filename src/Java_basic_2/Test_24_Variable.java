f0e9c61de0221b54ee463a5f1d48cf2d41b29cea
pc <gardiakshay@gmail.com>
2025-05-22 15:36:43 +0530

    Variable

---
+++ src/Java_basic_2/Test_24_Variable.java
@@ -0,0 +1,22 @@
+package Java_basic_2;
+
+public class Test_24_Variable {
+
+    public static void main(String[] args) {
+
+    // How this byte age = 10 stored in the memory
+        // Memory only stored 0 and 1 value in the memory
+        byte  age = 10;
+
+        System.out.println(age);
+    }
+
+    public static void main(int args) {
+
+        byte  age = 10;
+        System.out.println(age);
+
+    }
+
+
+}

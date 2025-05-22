ec9eeccec96c24e7907266313c609a52b0322d8f
pc <gardiakshay@gmail.com>
2025-05-22 17:12:42 +0530

    Variable

---
+++ src/Java_basic_2/Test_27_Finale_variable.java
@@ -0,0 +1,13 @@
+package Java_basic_2;
+
+public class Test_27_Finale_variable {
+    public static void main(String[] args) {
+        final int age = 25;
+        // if we use final keyword to variable then we cannot change the value of varibale during execution
+        // age = 30;  If we try to change the value error will throw
+
+
+        System.out.println(age);
+
+    }
+}

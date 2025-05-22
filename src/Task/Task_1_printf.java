03ec9068cb40997c0e8ed86a4fd4363ab219bae8
pc <gardiakshay@gmail.com>
2025-05-22 16:13:29 +0530

    Task

---
+++ src/Task/Task_1_printf.java
@@ -0,0 +1,38 @@
+package Task;
+
+public class Task_1_printf {
+    /*
+    Table of 5 you need to print, Only use the Printf
+    int num=5;
+    5x1 =5
+    5X2 = 10
+    5x10= 50
+     */
+    public static void main(String[] args) {
+
+        int num= 5;
+        System.out.printf("a value is %d", num);
+        System.out.println();
+        System.out.printf("a value is %d", num*2);
+        System.out.println();
+        System.out.printf("a value is %d", num*3);
+        System.out.println();
+        System.out.printf("a value is %d", num*4);
+        System.out.println();
+        System.out.printf("a value is %d", num*5);
+        System.out.println();
+        System.out.printf("a value is %d", num*6);
+        System.out.println();
+        System.out.printf("a value is %d", num*7);
+        System.out.println();
+        System.out.printf("a value is %d", num*8);
+        System.out.println();
+        System.out.printf("a value is %d", num*9);
+        System.out.println();
+        System.out.printf("a value is %d", num*10);
+
+
+    }
+
+
+}

de3e3550b99a384d3b48ac00d95b63a88ea0d115
pc <gardiakshay@gmail.com>
2025-05-22 15:59:23 +0530

    Printf

---
+++ src/Java_basic_2/Test_25_printF.java
@@ -0,0 +1,23 @@
+package Java_basic_2;
+
+public class Test_25_printF {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 20;
+        System.out.printf("Your variable is %d", a);
+        System.out.println();
+        System.out.printf("Your variable is %d", b);
+
+        // %d for int, short, long,byte - data type
+        // %s - String
+        // %f - float and double
+        // %b - boolean
+
+        // So basically  'a' value replace in %d.......%d = 10;
+        // Print f is giving the format of output
+
+
+
+
+    }
+}
---
+++ src/Java_basic_2/Test_26_printf.java
@@ -0,0 +1,15 @@
+package Java_basic_2;
+
+public class Test_26_printf {
+    public static void main(String[] args) {
+
+        int a= 10;
+        byte b= 20;
+
+        System.out.printf("a value is %d, b value is %d", a, b); // a and b value is converted into %d
+        System.out.println();
+        System.out.printf("Your value : %d, b value is %d", a, b);
+        System.out.println();
+        System.out.printf("a = %d , b= %d", a, b);;
+    }
+}
